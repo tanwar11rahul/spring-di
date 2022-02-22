f4dc70dcf543d11e27269fa28a65fd1b8c438362
sachinmewar <sachinmewar800@gmail.com>
2022-02-22 23:52:41 +0530

    removed service and profile annotation

--- src/main/java/com/sachin/pet/DogService.java
+++ src/main/java/com/sachin/pet/DogService.java
@@ -3,8 +3,7 @@ package com.sachin.pet;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile({"dog", "default"})
-@Service
+
 public class DogService implements PetService{
     @Override
     public String favPet() {
